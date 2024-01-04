6b4a3c8fb79d0f45b2700583f4c8856b3c5d7365
luckynee <81769081+luckynee@users.noreply.github.com>
2024-01-04 15:04:34 +0700

    Lucky-Test-Pathfinding

--- PRoS/Assets/Scripts/PlayerController.cs
+++ PRoS/Assets/Scripts/PlayerController.cs
@@ -59,7 +59,7 @@ public class PlayerController : MonoBehaviour
         var facingDir = new Vector3(animator.GetFloat("moveX"), animator.GetFloat("moveY"));
         var interactPos = transform.position + facingDir;
 
-        //Debug.DrawLine(transform.position, interactPos, Color.red, 1f);
+        Debug.DrawLine(transform.position, interactPos, Color.red, 1f);
 
         var collider = Physics2D.OverlapCircle(interactPos, 0.2f, interactableLayer);
         if (collider != null)
