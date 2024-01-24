e024dad5c76a0900b617a70b10ee345731d42412
luckynee <81769081+luckynee@users.noreply.github.com>
2024-01-24 15:10:17 +0700

    Tech-Lucky-Add-Audio

---
+++ PRoS/Assets/Scripts/ScriptableObject/AudioClipRefsSO.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class AudioClipRefsSO : ScriptableObject
+{
+    public AudioClip walkingInWoods;
+    public AudioClip[] creekWalkingInWoods;
+    public AudioClip walkingInSand;
+    public AudioClip walkingInStone;
+}
